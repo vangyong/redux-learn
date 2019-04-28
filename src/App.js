e4254cfec9fe2c3c3efa0679dc91ad70453aaba8
vangyong <vangyong@126.com>
2019-04-28 22:26:12 +0800

    修改

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    componentDidMount() {
+        console.log(this.props)
+    }
+
+    render() {
+        const {PayIncrease, PayDecrease} = this.props;
+        return (
+            <div className="App">
+                <h2>当月工资为{this.props.tiger}</h2>
+                <button onClick={PayIncrease}>升职加薪</button>
+                <button onClick={PayDecrease}>迟到罚款</button>
+            </div>
+        );
+    }
 }
 
-export default App;
+//需要渲染什么数据
+function mapStateToProps(state) {
+    return {
+        tiger: state
+    }
+}
+
+//需要触发什么行为
+function mapDispatchToProps(dispatch) {
+    return {
+        PayIncrease: () => dispatch({type: '涨工资'}),
+        PayDecrease: () => dispatch({type: '扣工资'})
+    }
+}
+
+export default App = connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux'
+import {Provider} from 'react-redux';
+import reducer from './index.reducer'
+
+//创建store
+const store = createStore(reducer);
+
+//订阅事件
+// store.subscribe(() =>
+//     console.log(store.getState())
+// );
+
+//派发事件
+// store.dispatch(increase)
+// console.log(store.getState())
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//需要渲染什么数据
+function mapStateToProps(state) {
+    return {
+        tiger: state
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//需要触发什么行为
+function mapDispatchToProps(dispatch) {
+    return {
+        PayIncrease: () => dispatch({type: '涨工资'}),
+        PayDecrease: () => dispatch({type: '扣工资'})
+    }
+}
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
